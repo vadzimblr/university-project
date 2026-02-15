c03e25c3f1943179c1472d1bd6bc7f515fbb05e4
vadzimblr <vadim.poznyak.wrk@gmail.com>
2026-02-15 15:13:08 +0300

    delete mock fe

--- frontend-mock/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './app/App.vue';
-import { router } from './app/router';
-import './styles/main.css';
-
-createApp(App).use(createPinia()).use(router).mount('#app');
